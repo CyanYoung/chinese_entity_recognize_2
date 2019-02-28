51f6e7bef7ef8a6c81b15fa9b57ff57c241f0fe3
yangjc <bupt_yjc@163.com>
2019-02-28 16:14:26 +0800

    change rate

--- nn_arch.py
+++ nn_arch.py
@@ -6,7 +6,7 @@ def rnn(embed_input, class_num):
     ba = Bidirectional(ra, merge_mode='concat')
     da = Dense(class_num, activation='softmax')
     x = ba(embed_input)
-    x = Dropout(0.5)(x)
+    x = Dropout(0.2)(x)
     return da(x)
 
 
@@ -14,5 +14,5 @@ def rnn_crf(embed_input, crf):
     ra = LSTM(200, activation='tanh', return_sequences=True)
     ba = Bidirectional(ra, merge_mode='concat')
     x = ba(embed_input)
-    x = Dropout(0.5)(x)
+    x = Dropout(0.2)(x)
     return crf(x)
